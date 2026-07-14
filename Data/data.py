6904e22b2de5a8e3da5909a4cb270753f7673c1a
Hassan <99041864+dalife001@users.noreply.github.com>
2026-07-14 20:35:50 +0300

    Data/data.py with processing stubs

---
+++ Data/data.py
@@ -0,0 +1,31 @@
+# this one gets the data from  mangodb aka the discord messages and it will clean it up and make sure it can be used as training mod
+
+
+def get_data():
+    return 
+
+def clean_data(data):
+    # clean the data here
+    return data
+
+
+def prepare_data():
+    data = get_data()
+    cleaned_data = clean_data(data)
+    return cleaned_data
+
+def save_data(data):
+    # save the data to a file or database
+    pass
+
+
+def main():
+    get_data()
+    prepared_data = prepare_data()
+    save_data(prepared_data)
+
+
+if __name__ == "__main__":
+
+    main()
+
